8e12bf94c08bc1bb76e68ee9337ba2b33e0a1e1d
anil kumbhar <anil_kumbhar@persistent.co.in>
2014-03-18 06:39:51 -0500

    Retrieved specified user details.
    Corrected getCurrentUser with getUsers with specified user id.

--- jumpgate/identity/drivers/sl/user.py
+++ jumpgate/identity/drivers/sl/user.py
@@ -1,17 +1,20 @@
+import logging
+LOG = logging.getLogger(__name__)
 
 
 class UserV2(object):
 
     def on_get(self, req, resp, user_id):
         client = req.env['sl_client']
-        account = client['Account'].getCurrentUser()
+        user = client['Account'].getUsers(
+            mask='mask[id,username,accountId]', filter={'users': {'id': {'operation': user_id}}})
+        if not user or not len(user):
+            raise Exception("User with ID '%s' Doesn't Exists" % user_id)
 
-        user = {'id': str(account['id']),
-                'name': str(account['firstName']),
-                'username': str(account['username']),
-                'email': str(account['email']),
-                'tenantId': str(account['accountId']),
-                'enabled': 'true'
-                }
-
-        resp.body = {'user': user}
+        user_response = {'id': str(user[0]['id']),
+                         'username': str(user[0]['username']),
+                         'email': str(user[0]['username']),
+                         'tenantId': str(user[0]['accountId']),
+                         'enabled': 'true'
+                         }
+        resp.body = {'user': user_response}
