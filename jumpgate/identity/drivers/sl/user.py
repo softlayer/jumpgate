09429ed95239e4ccc3020df7a50a72eec6713b34
anil k <anil@anil1.localdomain>
2014-03-20 09:06:56 -0500

    If user is unauthorized corresponding HTTP code/message has been given

--- jumpgate/identity/drivers/sl/user.py
+++ jumpgate/identity/drivers/sl/user.py
@@ -1,18 +1,21 @@
-from jumpgate.common.error_handling import not_found
+from jumpgate.common.error_handling import unauthorized,not_found
 
 
 class UserV2(object):
 
     def on_get(self, req, resp, user_id):
         client = req.env['sl_client']
-        user = client['Account'].getUsers(
-            mask='mask[id,username,accountId]', filter={'users': {'id': {'operation': user_id}}})
+        try:
+            user = client['Account'].getUsers(
+            mask='mask[id,username,accountId]', filter={'users': {'id': {'operation': user_id}}})    
+        except Exception:
+            return unauthorized(resp,"Unauthorized User to view given user")
         if not user or not len(user):
             return not_found(resp, "Invalid user ID specified.")
         user_response = {'id': str(user[0]['id']),
-                         'username': str(user[0]['username']),
-                         'email': str(user[0]['username']),
+                         'username': user[0]['username'],
+                         'email': user[0]['username'],
                          'tenantId': str(user[0]['accountId']),
-                         'enabled': 'true'
+                         'enabled': True
                          }
         resp.body = {'user': user_response}
