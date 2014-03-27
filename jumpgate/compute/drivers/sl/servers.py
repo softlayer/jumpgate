8d17fb3d08294dc418a4b06b9a1c410ee9726d28
boden <boden@linux.vnet.ibm.com>
2014-03-27 12:12:39 -0400

    support for default domain in jumpgate.conf
    adds support for the specification of the default domain to use when
    booting servers in the conf file rather than hardcoding it to
    'jumpgate.com' in the py code. this is a partial implementation of
    https://github.com/softlayer/jumpgate/issues/63 which allows
    configurable domain per jumpgate service. a future PR will include
    additional support such as that discussed in
    https://github.com/softlayer/jumpgate/issues/63

--- jumpgate/compute/drivers/sl/servers.py
+++ jumpgate/compute/drivers/sl/servers.py
@@ -2,6 +2,7 @@ import json
 
 from SoftLayer import CCIManager, SshKeyManager, SoftLayerAPIError
 
+from jumpgate.common.config import CONF
 from jumpgate.common.utils import lookup
 from jumpgate.common.error_handling import (bad_request, duplicate,
                                             compute_fault, not_found)
@@ -197,7 +198,7 @@ class ServersV2(object):
 
         payload = {
             'hostname': body['server']['name'],
-            'domain': 'jumpgate.com',  # TODO - Don't hardcode this
+            'domain': CONF['default_domain'] or 'jumpgate.com',
             'cpus': flavor['cpus'],
             'memory': flavor['ram'],
             'hourly': True,  # TODO - How do we set this accurately?
