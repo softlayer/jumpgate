7fee23ae07d6e29719e2d79a74d806a1f0f6032e
njain <njain@softlayer.com>
2014-04-07 16:08:54 -0500

    convert IDs to base 64 , add disk-type local/SAN, generate flavour details from id

--- jumpgate/compute/drivers/sl/flavors.py
+++ jumpgate/compute/drivers/sl/flavors.py
@@ -1,44 +1,108 @@
 from jumpgate.common.error_handling import bad_request, not_found
+import base64
+import re
 
 FLAVORS = {
     1: {
         'id': '1',
-        'name': '1 vCPU, 1GB ram, 25GB',
+        'name': '1 vCPU, 1GB ram, 25GB, local',
         'ram': 1024,
         'disk': 25,
+        'disk-type': 'Local',
         'cpus': 1,
     },
     2: {
         'id': '2',
-        'name': '1 vCPU, 1GB ram, 100GB',
+        'name': '1 vCPU, 1GB ram, 100GB, local',
         'ram': 1024,
         'disk': 100,
+        'disk-type': 'Local',
         'cpus': 1,
     },
     3: {
         'id': '3',
-        'name': '2 vCPU, 2GB ram, 100GB',
+        'name': '2 vCPU, 2GB ram, 100GB, local',
         'ram': 2 * 1024,
         'disk': 100,
+        'disk-type': 'Local',
         'cpus': 2,
     },
     4: {
         'id': '4',
-        'name': '4 vCPU, 4GB ram, 100GB',
+        'name': '4 vCPU, 4GB ram, 100GB, local',
         'ram': 4 * 1024,
         'disk': 100,
+        'disk-type': 'Local',
         'cpus': 4,
     },
     5: {
         'id': '5',
-        'name': '8 vCPU, 8GB ram, 100GB',
+        'name': '8 vCPU, 8GB ram, 100GB, local',
         'ram': 8 * 1024,
         'disk': 100,
+        'disk-type': 'Local',
+        'cpus': 8,
+    },
+    11: {
+        'id': '11',
+        'name': '1 vCPU, 1GB ram, 25GB, SAN',
+        'ram': 1024,
+        'disk': 25,
+        'disk-type': 'SAN',
+        'cpus': 1,
+    },
+    12: {
+        'id': '12',
+        'name': '1 vCPU, 1GB ram, 100GB, SAN',
+        'ram': 1024,
+        'disk': 100,
+        'disk-type': 'SAN',
+        'cpus': 1,
+    },
+    13: {
+        'id': '13',
+        'name': '2 vCPU, 2GB ram, 100GB, SAN',
+        'ram': 2 * 1024,
+        'disk': 100,
+        'disk-type': 'SAN',
+        'cpus': 2,
+    },
+    14: {
+        'id': '14',
+        'name': '4 vCPU, 4GB ram, 100GB, SAN',
+        'ram': 4 * 1024,
+        'disk': 100,
+        'disk-type': 'SAN',
+        'cpus': 4,
+    },
+    15: {
+        'id': '15',
+        'name': '8 vCPU, 8GB ram, 100GB, SAN',
+        'ram': 8 * 1024,
+        'disk': 100,
+        'disk-type': 'SAN',
         'cpus': 8,
     },
 }
+
+
+def encode_tob64_id(name):
+    return base64.b64encode((name.replace(" ", "")).replace(",", ":"))
+
+
+def decode_fromb64_string(idstr):
+    return base64.b64decode(idstr)
+
+
+def set_flavors_base64_id():
+    for flavor_id, flavor in FLAVORS.items():
+        flavor['id'] = encode_tob64_id(flavor['name'])
+
+
 # Set flavor '1' as the default
 FLAVORS[None] = FLAVORS[1]
+# Encode flavor ids
+set_flavors_base64_id()
 
 
 class FlavorV2(object):
@@ -124,6 +188,42 @@ def filter_flavor_refs(req, resp, flavor_refs):
 
 
 def get_flavor_details(app, req, flavor_ref, detail=False):
+    # extract details by decoding the id string
+    flavor = {
+        'id': flavor_ref['id'],
+        'links': [
+            {
+                'href': app.get_endpoint_url('compute', req, 'v2_flavor',
+                                             flavor_id=flavor_ref['id']),
+                'rel': 'self',
+            }
+        ],
+
+    }
+    idstr = decode_fromb64_string(flavor_ref['id'])
+    #id is delimited by :
+    s = idstr.split(":")
+    cpu= int(re.match(r'\d+', s[0]).group())
+    ram = int(re.match(r'\d+', s[1]).group())*1024
+    disk = int(re.match(r'\d+', s[2]).group())
+    disk_type = s[3]
+    names = "%d vCPU, %dGB ram, %dGB, %s" % (cpu, ram, disk, disk_type)
+    flavor['name'] = names
+    if detail:
+        flavor['disk'] = disk
+        flavor['ram'] = ram
+        flavor['vcpus'] = cpu
+        flavor['OS-FLV-DISK-TYPE:disk_type'] = disk_type
+        flavor['swap'] = ''
+        flavor['rxtx_factor'] = 1
+        flavor['os-flavor-access:is_public'] = True
+        flavor['OS-FLV-EXT-DATA:ephemeral'] = 0
+        flavor['OS-FLV-DISABLED:disabled'] = False
+
+    return flavor
+
+
+def get_flavor_details_old(app, req, flavor_ref, detail=False):
     flavor = {
         'id': flavor_ref['id'],
         'links': [
@@ -140,6 +240,7 @@ def get_flavor_details(app, req, flavor_ref, detail=False):
         flavor['disk'] = flavor_ref['disk']
         flavor['ram'] = flavor_ref['ram']
         flavor['vcpus'] = flavor_ref['cpus']
+        flavor['OS-FLV-DISK-TYPE:disk_type']= flavor_ref['disk-type']
         flavor['swap'] = ''
         flavor['rxtx_factor'] = 1
         flavor['os-flavor-access:is_public'] = True
@@ -147,3 +248,4 @@ def get_flavor_details(app, req, flavor_ref, detail=False):
         flavor['OS-FLV-DISABLED:disabled'] = False
 
     return flavor
+
