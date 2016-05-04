93029bb8c97908ed05bd1500ff9f220068b298f1
Michael O'Rourke <michael.orourke@zoho.com>
2016-05-03 21:54:53 -0500

    remove scriptTimeout and async

--- javascript/fonts.js
+++ javascript/fonts.js
@@ -8,7 +8,6 @@ WebFontConfig = {
   wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
     '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
   wf.type = 'text/javascript';
-  wf.async = 'true';
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(wf, s);
 })();
@@ -17,8 +16,7 @@ WebFontConfig = {
 
 (function() {
   var config = {
-    kitId: 'lzt3exy',
-    scriptTimeout: 500
+    kitId: 'lzt3exy'
   };
   var h=document.getElementsByTagName("html")[0];h.className+=" wf-loading";var t=setTimeout(function(){h.className=h.className.replace(/(\s|^)wf-loading(\s|$)/g," ");h.className+=" wf-inactive"},config.scriptTimeout);var tk=document.createElement("script"),d=false;tk.src='//use.typekit.net/'+config.kitId+'.js';tk.type="text/javascript";tk.async="true";tk.onload=tk.onreadystatechange=function(){var a=this.readyState;if(d||a&&a!="complete"&&a!="loaded")return;d=true;clearTimeout(t);try{Typekit.load(config)}catch(b){}};var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(tk,s)
 })();
